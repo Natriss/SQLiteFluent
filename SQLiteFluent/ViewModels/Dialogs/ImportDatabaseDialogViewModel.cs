02dec4cd611bafce21b63ee06035f5596f3d1f2a
Natriss <thibeau.ds1@outlook.com>
2023-12-28 21:28:01 +0100

    Update on the import

--- SQLiteFluent/ViewModels/Dialogs/ImportDatabaseDialogViewModel.cs
+++ SQLiteFluent/ViewModels/Dialogs/ImportDatabaseDialogViewModel.cs
@@ -1,4 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using Windows.Storage;
+using Windows.Storage.Pickers;
 
 namespace SQLiteFluent.ViewModels.Dialogs
 {
@@ -10,6 +14,38 @@ namespace SQLiteFluent.ViewModels.Dialogs
 		{
 			get { return _path; }
 			set { _path = value; OnPropertyChanged(Path); }
+			//TODO not updaing the UI
+		}
+
+		public IRelayCommand PickAFileCommand { get; private set; }
+
+		public ImportDatabaseDialogViewModel()
+		{
+			PickAFileCommand = new RelayCommand(PickAFile);
+		}
+
+		private async void PickAFile()
+		{
+			FileOpenPicker openPicker = new()
+			{
+				ViewMode = PickerViewMode.Thumbnail,
+				SuggestedStartLocation = PickerLocationId.DocumentsLibrary
+			};
+			openPicker.FileTypeFilter.Add(".db");
+
+			var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(App.Window);
+			WinRT.Interop.InitializeWithWindow.Initialize(openPicker, hWnd);
+
+			StorageFile file = await openPicker.PickSingleFileAsync();
+			if (file != null)
+			{
+				// Application now has read/write access to the picked file
+				Path = file.Name;
+			}
+			else
+			{
+				Path = "Operation cancelled.";
+			}
 		}
 	}
 }
